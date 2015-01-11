2899a97e873456a18977840ac6efb89e88697100
jsirol <johannes.sirola@outlook.com>
2015-01-11 20:30:36 +0200

    update

--- C/PE18.c
+++ C/PE18.c
@@ -8,64 +8,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-unsigned long max(unsigned long a,unsigned long b)
-{
-    if(a >= b) return a;
+unsigned long max(unsigned long a, unsigned long b) {
+    if (a >= b) return a;
     return b;
 }
 
+void max_path(int rows, char* fname) {
+    printf("Reading data...\n");
+    // initialize variables
+    clock_t t;
+    unsigned int row = 0, col = 0, i = 0, j = 0, curr = 0;
+    unsigned long **n = malloc(rows * sizeof (unsigned long*));
+    for (i = 0; i < rows; i++) {
+        n[i] = malloc((i+1) * sizeof (unsigned long));
+    }
+    t = clock();
+    
+    // get file size
+    FILE *f = fopen(fname, "r");
+    fseek(f, 0L, SEEK_END);
+    int size = ftell(f);
+    fseek(f, 0L, SEEK_SET);
 
-int main(int argc, char** argv) 
-{
-    unsigned int row = 0, col=0, count=0, i, j;
-    unsigned long **n = malloc(20000*sizeof(int*));
-    for(i=0; i< 20000; i++) 
-    {
-        n[i] = malloc(20000*sizeof(unsigned long));
+    // initialize and allocate space for buffer
+    char *buffer;
+    if ((buffer = malloc(size * sizeof (char))) == NULL) {
+        printf("memory allocation error!\n");
+        exit(0);
     }
-    FILE *f = fopen("asd3.txt","r");
-    while(!feof(f))
-    {
-        fscanf(f,"%d",&n[row][col]);
-        count++;
+    buffer = memset(buffer, 0, sizeof (buffer));
+    char *buffer_start = buffer;
+    
+    // read file to buffer
+    fread(buffer, sizeof (char), size, f);
+    // process buffer
+    while (curr = strtol(buffer, &buffer, 10)) {
+        n[row][col] = (unsigned long) curr;
         col++;
-        if(count == row + 1)
-        {
-            row++;
+        if (col > row) {
             col = 0;
-            count = 0;
-        }
-    }
-    for(i=1; i<20000; i++)
-    {
-        for(j=0; j<i; j++)
-        {
-            if(0 < j && j < i)
-            {
-                n[i][j] += max(n[i-1][j-1],n[i-1][j]);
-            } 
-            else if(j==0)
-            {
-                n[i][j] +=n[i-1][j];
-            }
-            else
-            {
-                n[i][j] += n[i-1][j-1];
+            row++;
+            if (row == rows) {
+                break;
             }
         }
     }
+    buffer = buffer_start;
+    free(buffer);
+    fclose(f);
+    t = clock() - t;
+    // table (n) for dynamic program constructed
+    // now calculate result
+    printf("Reading complete, took %f seconds\n", ((float) t) / CLOCKS_PER_SEC);
+    printf("Calculating result...\n");
+    for (i = 1; i < rows; i++) {
+        for (j = 0; j <= i; j++) {
+            if (0 < j && j < i) {
+                n[i][j] += max(n[i - 1][j - 1], n[i - 1][j]);
+            } else if (j == 0) {
+                n[i][j] += n[i - 1][j];
+            } else {
+                n[i][j] += n[i - 1][j - 1];
+            }
+        }
+    }
+    // find max value from the last row
     unsigned long mm = 0;
-    for(i=0; i<20000; i++)
-    {
-        mm = max(mm,n[19999][i]);
+    for (i = 0; i < rows; i++) {
+        mm = max(mm, n[rows - 1][i]);
     }
-    for(i=0; i < 20000; i++) 
-    {
+    t = clock() - t;
+    printf("Calculation complete, took %f seconds\n", ((float) t) / CLOCKS_PER_SEC);
+    printf("max route cost: %lu", mm);
+    // free allocated memory
+    for (i = 0; i < rows; i++) {
         free(n[i]);
     }
     free(n);
-    printf("max route cost: %lu",mm);
-    fclose(f);
+}
+
+int main(int argc, char** argv) {
+    char* fname = "pp.txt";
+    // set argument to be the number of rows and remember to change filename
+    max_path(15, fname); 
     return (0);
 }
